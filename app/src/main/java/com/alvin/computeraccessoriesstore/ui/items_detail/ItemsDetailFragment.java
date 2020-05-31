f1dc4a1fab6e7ed83e8870b937da3709c3c07745
sabohao <alvin.set99@gmail.com>
2020-05-31 12:37:23 +0700

    Redesign ItemsDetailFragment java & xml, make anim fade in out AppBarLayout

--- app/src/main/java/com/alvin/computeraccessoriesstore/ui/items_detail/ItemsDetailFragment.java
+++ app/src/main/java/com/alvin/computeraccessoriesstore/ui/items_detail/ItemsDetailFragment.java
@@ -1,19 +1,22 @@
 package com.alvin.computeraccessoriesstore.ui.items_detail;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.alvin.computeraccessoriesstore.Common.Common;
@@ -26,15 +29,14 @@ import com.alvin.computeraccessoriesstore.RoomDB.CartItem;
 import com.alvin.computeraccessoriesstore.RoomDB.LocalCartDataSource;
 import com.andremion.counterfab.CounterFab;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 import com.squareup.picasso.Picasso;
 
 import org.greenrobot.eventbus.EventBus;
-
-import java.util.Arrays;
-import java.util.Spliterator;
-import java.util.Spliterators;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,23 +54,35 @@ public class ItemsDetailFragment extends Fragment {
     private CartDataSource cartDataSource;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
+    @BindView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.rlTop)
+    RelativeLayout rlTop;
+    @BindView(R.id.llTop)
+    LinearLayout llTop;
     @BindView(R.id.imgItemsDetail)
     ImageView imgItemsDetail;
+    @BindView(R.id.imgItemsDetail2)
+    ImageView imgItemsDetail2;
     @BindView(R.id.btnCart)
     CounterFab btnCart;
     @BindView(R.id.itemsName)
     TextView itemsName;
+    @BindView(R.id.itemsName2)
+    TextView itemsName2;
     @BindView(R.id.itemsPrice)
     TextView itemsPrice;
+    @BindView(R.id.itemsPrice2)
+    TextView itemsPrice2;
     @BindView(R.id.number_button)
     ElegantNumberButton numberButton;
     @BindView(R.id.itemsDescription)
-    TextView itemsDescription;
+    ExpandableTextView itemsDescription;
     @BindView(R.id.itemsSpecification)
-    TextView itemsSpecification;
+    ExpandableTextView itemsSpecification;
 
     @OnClick(R.id.btnCart)
-    void cart(){
+    void cart() {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
@@ -96,7 +110,7 @@ public class ItemsDetailFragment extends Fragment {
 
                     @Override
                     public void onSuccess(CartItem cartItemFromDB) {
-                        if (cartItemFromDB.equals(cartItem)){
+                        if (cartItemFromDB.equals(cartItem)) {
                             // Already in database, just update
                             cartItemFromDB.setItemExtraPrice(cartItemFromDB.getItemExtraPrice());
                             cartItemFromDB.setItemQuantity(cartItemFromDB.getItemQuantity() + cartItem.getItemQuantity());
@@ -119,41 +133,38 @@ public class ItemsDetailFragment extends Fragment {
 
                                         @Override
                                         public void onError(Throwable e) {
-                                            Toast.makeText(getContext(), "[UPDATE CART]"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(getContext(), "[UPDATE CART]" + e.getMessage(), Toast.LENGTH_SHORT).show();
                                         }
                                     });
-                        }
-                        else {
+                        } else {
                             // Item not available in cart before, insert new
                             compositeDisposable.add(cartDataSource.insertOrReplaceAll(cartItem)
                                     .subscribeOn(Schedulers.io())
                                     .observeOn(AndroidSchedulers.mainThread())
-                                    .subscribe(() ->{
+                                    .subscribe(() -> {
                                         Toast.makeText(getContext(), "Add to Cart success", Toast.LENGTH_SHORT).show();
                                         EventBus.getDefault().postSticky(new CounterCartEvent(true));
                                     }, throwable -> {
-                                        Toast.makeText(getContext(), "[CART ERROR]"+throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getContext(), "[CART ERROR]" + throwable.getMessage(), Toast.LENGTH_SHORT).show();
                                     }));
                         }
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        if (e.getMessage().contains("empty"))
-                        {
+                        if (e.getMessage().contains("empty")) {
                             // Default, if Cart is empty, this code will be fired
                             compositeDisposable.add(cartDataSource.insertOrReplaceAll(cartItem)
                                     .subscribeOn(Schedulers.io())
                                     .observeOn(AndroidSchedulers.mainThread())
-                                    .subscribe(() ->{
+                                    .subscribe(() -> {
                                         Toast.makeText(getContext(), "Add to Cart success", Toast.LENGTH_SHORT).show();
                                         EventBus.getDefault().postSticky(new CounterCartEvent(true));
                                     }, throwable -> {
-                                        Toast.makeText(getContext(), "[CART ERROR]"+throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getContext(), "[CART ERROR]" + throwable.getMessage(), Toast.LENGTH_SHORT).show();
                                     }));
-                        }
-                        else
-                            Toast.makeText(getContext(), "[GET CART]"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        } else
+                            Toast.makeText(getContext(), "[GET CART]" + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
@@ -181,13 +192,51 @@ public class ItemsDetailFragment extends Fragment {
         Picasso.get().load(itemsModel.getImage()).into(imgItemsDetail);
         itemsName.setText(itemsModel.getName());
         itemsPrice.setText(new StringBuilder("IDR ").append(Common.formatPrice(Double.valueOf(itemsModel.getPrice()))));
+        itemsName2.setText(itemsModel.getName());
+        itemsPrice2.setText(new StringBuilder("IDR ").append(Common.formatPrice(Double.valueOf(itemsModel.getPrice()))));
         itemsDescription.setText(itemsModel.getDescription());
-        itemsSpecification.setText(itemsModel.getSpecification());
+
+        String string = itemsModel.getSpecification();
+        String str = string.replace("/n", "\n");
+
+        itemsSpecification.setText(str);
 
 
-        ((AppCompatActivity)getActivity())
+        ((AppCompatActivity) getActivity())
                 .getSupportActionBar()
-                .setTitle(Common.selectedItems.getName()) ;
+                .setTitle(Common.selectedItems.getName());
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+            Animation fadeIn = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in_imgdetail);
+            Animation fadeOut = AnimationUtils.loadAnimation(getContext(), R.anim.fade_out_imgdetail);
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                    //Toast.makeText(getContext(), String.valueOf(verticalOffset), Toast.LENGTH_SHORT).show();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    rlTop.setVisibility(View.VISIBLE);
+                    Picasso.get().load(itemsModel.getImage()).into(imgItemsDetail2);
+                    rlTop.startAnimation(fadeIn);
+                    llTop.startAnimation(fadeOut);
+                    llTop.setVisibility(View.GONE);
+                    llTop.clearAnimation();
+                    isShow = true;
+                } else if (isShow) {
+
+                    rlTop.startAnimation(fadeOut);
+                    llTop.startAnimation(fadeIn);
+                    rlTop.setVisibility(View.GONE);
+                    llTop.setVisibility(View.VISIBLE);
+
+                    isShow = false;
+                }
+            }
+        });
     }
 
     @Override
